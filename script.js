70ae3f8e5ecbe2230632f384518d1fa7f3552d93
kisuke-sama <theobizet@outlook.fr>
2025-01-23 12:14:29 +0100

    #

--- script.js
+++
@@ -1,9 +0,0 @@
-function showSection(id) {
-    document.querySelectorAll('section').forEach(section => {
-        section.classList.remove('active');
-    });
-    document.getElementById(id).classList.add('active');
-}
-
-// Show the home section by default
-showSection('home');
