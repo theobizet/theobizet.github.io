b7a98f2eeb018a13495d74a6da578fff273b8959
theobizet <theobizet@outlook.fr>
2025-01-28 12:33:42 +0100

    #

--- script.js
+++ script.js
@@ -29,3 +29,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }, { threshold: 0.5 });
     skills.forEach(skill => observer.observe(skill));
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    let hash = window.location.hash; // Récupère la partie hash de l'URL
+    if (!hash) {
+        hash = '#home'; // Définit le hash par défaut à #home si l'URL ne contient pas de hash
+        history.replaceState(null, null, hash); // Met à jour l'URL avec le hash par défaut
+    }
+    const tabTrigger = document.querySelector(`a[href="${hash}"]`);
+    if (tabTrigger) {
+        const tab = new bootstrap.Tab(tabTrigger);
+        tab.show();
+    }
+
+    // Ajoute un gestionnaire d'événements pour les clics sur les liens d'onglets
+    const tabLinks = document.querySelectorAll('.nav-link[data-bs-toggle="pill"]');
+    tabLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            const targetId = link.getAttribute('href');
+            history.pushState(null, null, targetId); // Modifie le hash dans l'URL
+        });
+    });
+});
